50c4d48153f3a7cb8b96844a53cc38e359b64b2e
Marc Brinkmann <git@marcbrinkmann.de>
2012-06-30 16:32:21 +0200

    Switched data to XML.

--- pmt2.py
+++ pmt2.py
@@ -1,190 +1,94 @@
-from pymeta.grammar import OMetaGrammar
-from pymeta.runtime import ParseError
+import sys
+from xml.etree import cElementTree as ET
 
 from PySide import QtCore
 
-import sys
+
+def first_valid(*args):
+    for a in args:
+        if None != a:
+            return a
 
 
-# primitives
-definition = """
-escaped_char = '\\\\' ('"' | '\\\\'):c
-  -> c
-name = (letter | "_"):c (letterOrDigit | "-")*:cs
-  -> c + ''.join(cs)
-word_string = (~' ' anything)*:w
-  -> ''.join(w)
-string = '"' (escaped_char | ~('"') anything)*:cs '"'
-  -> ''.join(cs)
-any_string = (string | word_string)
-basic_int =  digit+:ds
-  -> int(''.join(ds), 10)
-float = digit+:fs '.' digit+:ss
-  -> float(''.join(fs + ['.'] + ss))
-eol_string = (~vspace anything)*:cs
-  -> ''.join(cs).strip()
+def read_xml_shape(elem, context={}):
 
-tuple2 = '(' hspace* float:a hspace* float:b hspace* ')'
-  -> (a, b)
-point = tuple2:t
-  -> QPointF(*t)
-tuple4 = '(' hspace* float:a hspace* float:b hspace*\
-         float:c hspace* float:d hspace* ')'
-  -> (a, b, c, d)
-"""
+    if elem.tag == 'rect':
+        x = float(first_valid(elem.attrib.get('x'), context.get('rect-x')))
+        y = float(first_valid(elem.attrib.get('y'), context.get('rect-y')))
+        w = float(first_valid(elem.attrib.get('w'), context.get('rect-w')))
+        h = float(first_valid(elem.attrib.get('h'), context.get('rect-h')))
 
-# shapes
-definition += """
-rect = "rect" hspace* tuple4:t
-  -> QRectF(*t)
-shape = rect
-"""
+        return QtCore.QRectF(x, y, w, h)
 
-# main stuff
-definition += """
-header_line = hspace* name:k hspace* ':' hspace*\
-              (float|basic_int|eol_string):v hspace* vspace
-  -> (k, v)
-headers = header_line*:hs
-  -> dict(hs)
-
-rule = basic_int:k (hspace* ~shape any_string)*:ls hspace* shape:s
-  -> Rule(k, ls, s)
-row_key = hspace* basic_int:k (hspace* string)+:ls hspace* float?:f
-  -> (k, ls, f)
-row_rule = "row" hspace* point:rc row_key:k1 (',' row_key)*:ks
-  -> RowRule(rc, [k1] + ks)
-rule_line = (rule | row_rule):r vspace
-  -> r
-rules = rule_line*:rs
-  -> rs
-
-grammar = headers:hs emptyline rules:rs spaces end
-  -> PhysicalKeyboard(hs, rs)
-"""
+    raise ValueError('Unknown shape %s' % elem.tag)
 
 
-def preprocess(src):
-    class ReaderIter(object):
-        def __init__(self, src):
-            self.i = iter(src)
-            self.lineno = 0
-            self.col = 0
-            self.c = None
-
-        def next(self):
-            if '\n' == self.c:
-                self.lineno += 1
-                self.col = 0
-            else:
-                self.col += 1
-
-            self.c = self.i.next()
-
-            return self.c
-
-        def skip_until_eol(self):
-            while '\n' != self.c:
-                self.c = self.i.next()
-
-        def __iter__(self):
-            return self
-
-    rd = ReaderIter(src)
-    inside_string = False
-
-    while True:
-        c = rd.next()
-
-        if '\\' == c:
-            yield '\\'
-            yield rd.next()
-        elif '"' == c:
-            inside_string = not inside_string
-            yield c
-        elif inside_string:
-            yield c
-        elif '#' == c:
-            if rd.col != 0:
-                yield '\n'
-            rd.skip_until_eol()
-        else:
-            yield c
-
-
-class Rule(object):
-    def __init__(self, keycode, labels, shape):
-        self.keycode = keycode
-        self.labels = labels
-        self.shape = shape
-
-    def to_keys(self, kb):
-        return [Key(self.keycode, self.labels, self.shape)]
-
-    def __repr__(self):
-        return '%s(%r, %r, %r)' % (self.__class__.__name__,
-                                   self.keycode,
-                                   self.labels,
-                                   self.shape)
-
-
-class RowRule(object):
-    def __init__(self, coords, row_keys):
-        self.coords = coords
-        self.row_keys = row_keys
-
-    def to_keys(self, kb):
-        def_height = kb.get_conf('default-key-height')
-        def_width = kb.get_conf('default-key-width')
-        def_gap = kb.get_conf('default-key-gap')
-        ks = []
-
-        cur_offset = 0
-        for i, (keycode, labels, key_width) in enumerate(self.row_keys):
-            w = key_width or def_width
-            shape = QtCore.QRectF(
-                self.coords + QtCore.QPointF(cur_offset, 0),
-                QtCore.QSizeF(def_height, w)
-            )
-            ks.append(Key(keycode, labels, shape))
-            cur_offset += def_gap + w
-        return ks
-
-    def __repr__(self):
-        return '%s(%r, %r)' % (self.__class__.__name__,
-                               self.coords,
-                               self.row_keys)
+def read_xml_key(elem, context={}):
+    return Key(
+        keycode=int(elem.attrib['keycode']),
+        labels=[l.text for l in elem.iterfind('label')],
+        shape=read_xml_shape(elem.find('shape')[0], context)
+    )
 
 
 class PhysicalKeyboard(object):
     default_headers = {
-        'name': 'Unnamed keyboard',
+        'name': u'Unnamed keyboard',
         'default-key-gap': 0.1,
         'default-key-width': 1.8,
         'default-key-height': 1.8
     }
 
-    def __init__(self, headers, rules):
+    def __init__(self, headers, keys):
         self.headers = headers
-        self.rules = rules
+        self.keys = keys
 
         self.update_keys()
 
-    def get_conf(self, k):
-        try:
-            return self.headers[k]
-        except KeyError:
-            return self.default_headers[k]
-
     def update_keys(self):
-        self.keys = []
-        for rule in self.rules:
-            self.keys.extend(rule.to_keys(self))
+        self._keycode_keys = {key.keycode: key for key in self.keys}
 
     def __repr__(self):
         return '%s(%r, %r)' % (self.__class__.__name__,
                                self.headers,
-                               self.rules)
+                               self.keys)
+
+    @classmethod
+    def from_xml(cls, source):
+        headers = cls.default_headers.copy()
+
+        keys = []
+
+        et = ET.parse(source)
+        root = et.getroot()
+
+        # parse config
+        for conf in root.find('head'):
+            name = conf.attrib.get('name')
+            value = type(cls.default_headers.get(name, u''))(conf.text)
+
+            headers[name] = value
+
+        context = {
+            'rect-w': headers['default-key-width'],
+            'rect-h': headers['default-key-height']
+        }
+
+        for rule in root.find('keys'):
+            if rule.tag == 'key':
+                keys.append(read_xml_key(rule, context))
+            elif rule.tag == 'row':
+                row_context = context.copy()
+                row_context['rect-x'] = float(rule.attrib['x'])
+                row_context['rect-y'] = float(rule.attrib['y'])
+
+                for k in rule.iterfind('key'):
+                    key = read_xml_key(k, row_context)
+                    keys.append(key)
+                    row_context['rect-x'] += headers['default-key-gap'] +\
+                                             key.get_width()
+
+        return cls(headers, keys)
 
 
 class Key(object):
@@ -193,6 +97,9 @@ class Key(object):
         self.labels = labels
         self.shape = shape
 
+    def get_width(self):
+        return self.shape.width()
+
     def __repr__(self):
         return '%s(%r, %r, %r)' % (self.__class__.__name__,
                                    self.keycode,
@@ -200,23 +107,5 @@ class Key(object):
                                    self.shape)
 
 
-Grammar = OMetaGrammar.makeGrammar(definition, {
-    'QRectF': QtCore.QRectF,
-    'QPointF': QtCore.QPointF,
-    'Rule': Rule,
-    'RowRule': RowRule,
-    'PhysicalKeyboard': PhysicalKeyboard
-})
-
-
-try:
-    buf = ''.join(preprocess(sys.stdin.read()))
-    print buf
-    print '*' * 20
-    kb = Grammar.parse(buf)
-    print repr(kb)
-    print
-    for k in kb.keys:
-        print k
-except ParseError, e:
-    print e
+kb = PhysicalKeyboard.from_xml(sys.stdin)
+print kb
